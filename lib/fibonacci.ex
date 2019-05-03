f94aea70a0868b42d490a08f66111b5c407e3039
Ahmed Shalaby <kingshalaby@yahoo.com>
2019-05-03 22:51:50 +0200

    calculate fibonacci

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -6,4 +6,10 @@ defmodule Fibonacci do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def calculate(1), do: 1
+  def calculate(2), do: 1
+  def calculate(n) do
+    calculate(n-1) + calculate (n-2)
+  end
 end
---
+++ test/fibonacci_test.exs
@@ -0,0 +1,24 @@
+defmodule FibonacciTest do
+  use ExUnit.Case, async: true
+
+  describe "calculate/1 " do
+    test "it returns 1 when given 1" do
+      assert Fibonacci.calculate(1) === 1
+    end
+
+    test "it returns 1 when given 2" do
+      assert Fibonacci.calculate(2) === 1
+    end
+
+    test "it returns fibonacci number" do
+      assert Fibonacci.calculate(10) === 55
+    end
+
+    # this test is kinda fork bomb, my poor memory can not handle it
+    # test "it returns 354224848179261915075 given 100" do
+    #   assert Fibonacci.calculate(100) === 354224848179261915075
+    # end
+  end
+end
+
+
